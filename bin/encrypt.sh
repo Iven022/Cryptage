fd3a3b3811f324a2525c4b673fde334cee5589bc
Iven022 <61411888+Iven022@users.noreply.github.com>
2020-09-20 14:24:36 +0400

    Update encrypt.sh

--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -5,16 +5,16 @@ do
 	cd ~/src
 	dir=$(pwd)
 
-	if [ "$(ls -A /home/iven/src/Cryptage/ToEncrypt)" ]; then
-		for file in $dir/Cryptage/ToEncrypt/*; do
-	    		txt="$(basename "$file")"
-			mv $dir/Cryptage/ToEncrypt/$txt $dir/Cryptage/ToEncrypt/tocrypt.txt
+	if [ "$(ls -A /home/iven/src/Cryptage/ToEncrypt)" ]; then #checking if folder have content
+		for file in $dir/Cryptage/ToEncrypt/*; do #passing through each file
+	    		txt="$(basename "$file")" #saving file name in variable
+			mv $dir/Cryptage/ToEncrypt/$txt $dir/Cryptage/ToEncrypt/tocrypt.txt #renaming the file for the .py code to not bug
 		
 			cd $dir/Cryptage/bin
-			python3 encrypt.py
+			python3 encrypt.py #running the .py encryption code
 
 			cd $dir/Cryptage/ToEncrypt
-			rm $txt
+			rm $txt #deleting the file
 		done
 	fi
 done
